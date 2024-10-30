b52d87dde63abadd923fa8247e944da8cc50b350
Lilla03 <nguyenthuphuong3958@gmail.com>
2024-10-30 13:44:40 +0700

    change bg-image into-slicer

--- src/components/User/Intro_slicer.vue
+++ src/components/User/Intro_slicer.vue
@@ -1,48 +1,42 @@
 <template>
-    <div class="container my-5">
+  <div class="container my-5">
     <div class="lazy-title">
-        <h4 class="text-uppercase text-center ">Danh mục nổi bật</h4>
+      <h4 class="text-uppercase text-center">Danh mục nổi bật</h4>
     </div>
     <div id="catalog-lazy">
-        <div class="row text-uppercase text-center">
-            <div class="col-md-4">
-                    <div class=" catalog-item__t-shirt">
-                    <div class="banner-content ">
-                        <div class=" row d-flex justify-content-center">
-                            <a href="" class="banner-title">T-shirt</a>
-                            <a href="" class="btn-outline-white">mua ngay</a>
-                        </div>
-
-                    </div>
-                </div>
+      <div class="row text-uppercase text-center">
+        <div class="col-md-4">
+          <div class="catalog-item__t-shirt">
+            <div class="banner-content">
+              <div class="row d-flex justify-content-center">
+                <a href="" class="banner-title">Shirt</a>
+                <a href="" class="btn-outline-white">mua ngay</a>
+              </div>
             </div>
-            <div class="col-md-4">
-                <div class="catalog-item__jacket">
-                    <div class="banner-content">
-                        <div class=" row d-flex justify-content-center">
-                            <a href="" class="banner-title">Jacket</a>
-                            <a href="" class="btn-outline-white">mua ngay</a>
-                        </div>
-                    </div>
-            </div>
-            </div>
-            <div class="col-md-4"> 
-                <div class="catalog-item__top">
-                    <div class="banner-content ">
-                        <div class=" row d-flex justify-content-center">
-                        
-                            <a href="" class="banner-title">Top</a>
-                            <a href="" class="btn-outline-white"> mua ngay</a>
-                    </div>
-                </div>
-
-            </div>
-            </div>
+          </div>
         </div>
-        
+        <div class="col-md-4">
+          <div class="catalog-item__jacket">
+            <div class="banner-content">
+              <div class="row d-flex justify-content-center">
+                <a href="" class="banner-title">Jacket</a>
+                <a href="" class="btn-outline-white">mua ngay</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="catalog-item__top">
+            <div class="banner-content">
+              <div class="row d-flex justify-content-center">
+                <a href="" class="banner-title">Pant</a>
+                <a href="" class="btn-outline-white"> mua ngay</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
+  </div>
 </template>
-<style scoped>
-
-</style>
+<style scoped></style>
